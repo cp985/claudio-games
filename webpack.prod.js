b4aae54ff55b3ca121f7aaac47e7388ec07cba6c
cp985 <166384434+cp985@users.noreply.github.com>
2025-07-27 21:37:07 +0200

    aggiornato pagina su github

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "app.[contenthash].js",
-    publicPath: "/",
+    publicPath: "/claudio-games/",
     clean: true,
   },
 
