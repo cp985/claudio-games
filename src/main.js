d03319f0b54780e00f699cbb30181f9106c698e9
cp985 <166384434+cp985@users.noreply.github.com>
2025-07-30 11:45:01 +0200

    ultimato herobanner2

--- src/main.js
+++ src/main.js
@@ -4,19 +4,27 @@ import "./services/api.js";
 import "./style/resetcss.css";
 import "./style/body.background.css";
 
-document.addEventListener("DOMContentLoaded", () => {
-import(/* webpackChunkName: "carousel" */ './services/carousel-hero.js')
-  .then(module => {
-    console.log("Modulo:", module);
-    if (module.default) {
-      module.default();
-    } else {
-      console.error("nextSlide non trovata nel modulo");
-    }
-  });
-})
+window.addEventListener("load", () => {
+  console.log(
+    "Finestra completamente caricata, inclusi tutti gli stili e le immagini"
+  );
 
-
+  // Aggiungiamo un ulteriore ritardo per sicurezza
+  setTimeout(() => {
+    import(/* webpackChunkName: "carousel" */ "./services/carousel-hero.js")
+      .then((module) => {
+        console.log("Modulo carosello caricato");
+        if (module.default && typeof module.default === "function") {
+          module.default();
+        } else {
+          console.error("Funzione carosello non trovata nel modulo");
+        }
+      })
+      .catch((error) => {
+        console.error("Errore nel caricamento del carosello:", error);
+      });
+  }, 100);
+});
 
 if (module.hot) {
   module.hot.accept();
@@ -26,25 +34,8 @@ console.log("hello");
 
 //#region dom const
 
-// const slide = document.querySelectorAll(".slide");
-
 //#endregion dom const
 
 //#region dom handler
-// let slideActive = 0;
-
-// slide[0].classList.add("active");
-
-// function nextSlide() {
-//  slide[slideActive].classList.remove("active");
-// slideActive++;
-// if (slideActive >= slide.length) {
-//     slideActive = 0;
-//   }
-// slide[slideActive].classList.add("active");
-
-// }
-
-// setInterval(nextSlide, 5000);
 
 //#endregion dom handler
