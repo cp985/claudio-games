57a7e2fa8e170e4e3407ab8333d285a9e744b1f2
cp985 <166384434+cp985@users.noreply.github.com>
2025-07-30 20:40:41 +0200

    ultime modifiche carousel

--- src/services/carousel-hero.js
+++ src/services/carousel-hero.js
@@ -1,24 +1,40 @@
-//#region dom const
+// #region swiper carousel slider auto + button
 
-//#endregion dom const
+//swiper carousel 
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-//#region dom handler
 
-export default function nextSlide() {
- const slide = document.querySelectorAll(".slide");
-  let slideActive = 0;
-  slide[0].classList.add("active");
 
-  slideActive++;
-  function slideActiveHandler() {
-    slide[slideActive].classList.remove("active");
-    slideActive++;
-    if (slideActive >= slide.length) {
-      slideActive = 0;
-    }
-    slide[slideActive].classList.add("active");
-  }
-  setInterval(slideActiveHandler, 5000);
-}
 
-//#endregion dom handler
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination, Autoplay],
+  
+  // Configurazione base
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  
+  // Autoplay (opzionale)
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  
+  // Bottoni di navigazione
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  // Paginazione (opzionale)
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+// #endregion swiper slide auto + button
---
+++ temp_build/webpack.prod.js
@@ -0,0 +1,154 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[contenthash].js",
+    chunkFilename: "js/[name].[contenthash].chunk.js",
+    publicPath: "/claudio-games/",
+    clean: true,
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage",
+                  corejs: 3,
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/images/[name].[hash][ext]",
+        },
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.(mp3|wav|ogg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/audio/[name].[hash][ext]",
+        },
+      },
+
+      {
+        test: /\.(mp4|webm)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/video/[name].[hash][ext]",
+        },
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/fonts/[name].[hash][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+      chunkFilename: "css/[id].[contenthash].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "assets/img", to: "img" },
+        { from: "assets/favicon", to: "favicon" },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: [".js"],
+  },
+};
