19900098c2e43dc0fcd0546b141cf93efab9459b
cp985 <166384434+cp985@users.noreply.github.com>
2025-07-30 16:32:03 +0200

    debug load css-js

--- src/main.js
+++ src/main.js
@@ -17,60 +17,6 @@ window.onload = async function loadCarousel() {
   }
 };
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   console.log(
-//     "Finestra completamente caricata, inclusi tutti gli stili e le immagini"
-//   );
-
-//   setTimeout(() => {
-//     import(/*webpackChunkName: "carousel" */ "./services/carousel-hero.js")
-//       .then((module) => {
-//         console.log("Modulo carosello caricato");
-//         if (module.default && typeof module.default === "function") {
-//           module.default();
-//         } else {
-//           console.error("Funzione carosello non trovata nel modulo");
-//         }
-//       })
-//       .catch((error) => {
-//         console.error("Errore nel caricamento del carosello:", error);
-//       });
-//   }, 1000);
-// });
-
-// function waitForCss(callback) {
-//   function check() {
-//     if (document.body.getAttribute('data-css-loaded') === 'true') {
-//       console.log('CSS caricati, procedo con il carosello');
-//       callback();
-//     } else {
-//       console.log('CSS non ancora caricati, attendo...');
-//       setTimeout(check, 100);
-//     }
-//   }
-
-//   if (document.readyState === 'loading') {
-//     document.addEventListener('DOMContentLoaded', check);
-//   } else {
-//     check();
-//   }
-// }
-
-// waitForCss(function() {
-//   console.log('Caricamento del carosello...');
-//   import( /'*' webpackChunkName: "carousel" */ './services/carousel-hero.js')
-//     .then(module => {
-//       console.log('Modulo carosello caricato');
-//       if (module.default && typeof module.default === 'function') {
-//         module.default();
-//       } else {
-//         console.error('Funzione carosello non trovata nel modulo');
-//       }
-//     })
-//     .catch(error => {
-//       console.error('Errore nel caricamento del carosello:', error);
-//     });
-// });
 
 if (module.hot) {
   module.hot.accept();
